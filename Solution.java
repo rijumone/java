14fafdcc5898f6f7e86713ca923d2c1af021467f
Riju <mailmeonriju@gmail.com>
2017-09-04 23:58:56 +0530

    this is a stupid language

---
+++ Solution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args){
+
+   		Scanner scan = new Scanner(System.in);
+        // int i = scan.nextInt();
+
+        while(scan.hasNext()){
+        	System.out.print(scan.nextLine());	
+        }
+        
+
+	}
+
+}
+
